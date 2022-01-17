8778acc0f2966f85a4b8552a5a06dde894461fa7
Maxime <maxime.brossard5@gmail.com>
2022-01-17 08:53:01 +0100

    Working Shell

--- src/apps/turboShell.cpp
+++ src/apps/turboShell.cpp
@@ -8,12 +8,12 @@
 
 namespace turbo::shell{
 
-    void parse(char* cmd,char* arg){
+    void parse(char* cmd){
         switch(hash(cmd)){
             case hash("turbo"):
-                printf("https://www.instagram.com/http.doggyboys.fr/");
+                printf("https://www.instagram.com/http.doggyboys.fr/\n");
                 break;
-            case(hash("help")):
+            case hash("help"):
                 printf("All the listed command are not totally implement !\n");
                 printf("-turbo --Display the link to access to the Instagram account of the DoggyBoys !\n");
                 printf("-help --Display all the implement commands and future implemented command !\n");
@@ -27,20 +27,7 @@ namespace turbo::shell{
         {
             printf("root@turboShell: ");
             char *command = turbo::keyboard::getLine();
-            char cmd[15] = "\0";
-
-            for(size_t i= 0;i<strlen(command);i++){
-                if(command[i] != ' ' && command[i] != '\0'){
-                    char c[2] ="\0";
-                    c[0] = command[i];
-                    strcat(cmd,c);
-                }
-                else break;
-            }
-            char *arg= strrm(command,cmd);
-            arg=strrm(arg," ");
-
-            parse(cmd,arg);
+            parse(command);
         }
         
     }
