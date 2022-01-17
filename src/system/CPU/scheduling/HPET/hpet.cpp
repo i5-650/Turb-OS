3e7b47b20123a28aa6fd97959f2a077f278583e0
i5-650 <77734008+i5-650@users.noreply.github.com>
2022-01-17 15:02:29 +0100

    bypass terminal bug

--- src/system/CPU/scheduling/HPET/hpet.cpp
+++ src/system/CPU/scheduling/HPET/hpet.cpp
@@ -1,34 +1,45 @@
 #include <lib/memory/mmio.hpp>
-namespace turbo::system::CPU::scheduling::HPET{
-    bool isInit false;
-    static uint32_t clock = 0;
-    HPET* hpet;
+#include <system/CPU/scheduling/HPET/hpet.hpp>
+#include <system/ACPI/acpi.hpp>
+#include <drivers/display/serial/serial.hpp>
 
-    uint64_t counter (){
-        return mminq(&hpet->mainCounterValue);
-    }
-    void sleep (uint64_t uSeconds){//micro seconds
-        uint64_t target = counter() + (uSeconds * 1000000000) / clock;
-        while(counter()<target);
-    }
-    void init(){
-        turbo::serial::log("HPET : init start\n");
-        if (isInit){
-           turbo::serial::log("HPET : already initialised\n");
-           return; 
-        }
-        else if (!acpi::hpethdr){
-            turbo::serial::log("HPET : table not found\n");
-            return;
-        }
-        hpet=(HPET*)(acpi::hpethdr->address.address);
-        clock = hpet->generalCapabilities >> 32;
-        mmoutq(&hpet->generalConfiguration,0);
-        mmoutq(&hpet->mainCounterValue,0);
-        mmoutq(&hpet->generalConfiguration,1);
-        serial::newline();
-        isInit=true;
-    }
+
+namespace turbo::hpet{
+	bool isInit = false;
+	static uint32_t clock = 0;
+	HPET* hpet;
+
+	uint64_t counter (){
+		return mminq(&hpet->mainCounterValue);
+	}
+
+	void sleep (uint64_t uSeconds){ //micro seconds
+		uint64_t target = counter() + (uSeconds * 1000000000) / clock;
+		while(counter()<target);
+	}
+	
+	void init(){
+		turbo::serial::log("HPET : init start\n");
+
+		if (isInit){
+		   turbo::serial::log("HPET : already initialised\n");
+		   return; 
+		}
+		else if (!acpi::hpethdr){
+			turbo::serial::log("HPET : table not found\n");
+			return;
+		}
+
+		hpet = (HPET*)(acpi::hpethdr->address.address);
+		clock = hpet->generalCapabilities >> 32;
+
+		mmoutq(&hpet->generalConfiguration,0);
+		mmoutq(&hpet->mainCounterValue,0);
+		mmoutq(&hpet->generalConfiguration,1);
+
+		serial::newline();
+		isInit = true;
+	}
 }
 
 // Programmable Interval Timer
