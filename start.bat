41be398e1ab9b1db23ece0baad97d30f8a40bd86
i5-650 <l.perrin@proton.me>
2024-06-16 23:46:37 +0200

    commit for CyberPunk CTF

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-wsl.exe make -j$(nproc --all)
+wsl.exe make -j
 qemu-system-x86_64 -M q35 -cpu max -smp 4 -m 512M -boot d -rtc base=localtime -serial stdio -drive format=raw,file=disk.img -no-reboot -no-shutdown -d int -D log.txt -machine smm=off -cdrom image.iso
